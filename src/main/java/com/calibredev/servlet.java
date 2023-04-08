4fbfc15c9f74d7c3922c7b873a630a4b554c0d12
Andy Nguyen <andynguyen0114@gmail.com>
2023-04-08 02:41:27 -0500

    Java Servlet Web App

---
+++ src/main/java/com/calibredev/servlet.java
@@ -0,0 +1,45 @@
+package com.calibredev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servlet extends HttpServlet {
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws
+    IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+           
+            out.println("<html>");
+            out.println("<h1>Hello all from servlet</h1>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            String user = request.getParameter("user");
+            out.println("<h2> Welcome " + user + "</h2>");
+            out.print("<body>");
+            out.println("</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            out.close();
+        }
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+        processRequest(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+        processRequest(request, response);
+    }
+
+}
